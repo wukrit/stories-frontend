bdeea27ebecf61d49a1dfc596d121c7e7ba44f2e
wukrit <sukritwalia@gmail.com>
2019-10-02 15:16:13 -0400

    Created article view, and added auto scroll

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const articleListContainer = document.querySelector("#article-list-container")
 const topicContainer = document.querySelector("#topic-container")
 const topicList = document.querySelector("#topic-list")
 const articleList = document.querySelector("#article-list")
+const articleView = document.querySelector("#article-view")
 let selectedColumn = null
 let cachedTopics = null
 
@@ -57,36 +58,83 @@ const createArticleList = topic => {
 
     topic.articles.map(article => {
         if (tiles < 20) {
-            checkArticleCols(article)
+            checkArticleCols(article, topic)
             tiles++
         } else {
-            checkArticleCols(article, true)
+            checkArticleCols(article, topic, true)
             tiles = 0
         }
     })
+    articleList.scrollIntoView(true)
 }
 
-const checkArticleCols = (article, newCol = false) => {
+const checkArticleCols = (article, topic, newCol = false) => {
     if (newCol) {
         selectedColumn = createTileColumn("articles", articleList)   
     }
-    createArticleTile(article)
+    createArticleTile(article, topic)
 }
 
-const createArticleTile = article => {
+const createArticleTile = (article, topic) => {
     const newTile = document.createElement("div")
     newTile.classList.add("tile", "is-child", "box")
     newTile.dataset.id = article.id
+    newTile.dataset.topicId = topic.id
+
+    const articleTitle = document.createElement("p")
+    articleTitle.classList.add("title")
+    articleTitle.dataset.id = article.id
+    articleTitle.dataset.topicId = topic.id
+    articleTitle.innerText = article.title
+
+    const articleSource = document.createElement("p")
+    articleSource.classList.add("subtitle")
+    articleSource.dataset.id = article.id
+    articleSource.dataset.topicId = topic.id
+    articleSource.innerText = article.source
+    
+    const articleDesc = document.createElement("p")
+    articleDesc.dataset.id = article.id
+    articleDesc.dataset.topicId = topic.id
+    articleDesc.innerText = article.description
+    
+    const articleDate = document.createElement("p")
+    articleDate.dataset.id = article.id
+    articleDate.dataset.topicId = topic.id
+    articleDate.innerText = article.published_at
+
+    newTile.append(articleTitle, articleSource, articleDesc, articleDate)
+    selectedColumn.append(newTile)
+    
+    newTile.addEventListener("click", handleArticleClick)
+} 
+
+const createArticleView = article => {
+    const articleLink = document.createElement("a")
+    articleLink.href = article.url
+    articleLink.target = "_blank"
+    articleLink.dataset.id = article.id
+
+    const selectedArticleTile = document.createElement("div")
+    selectedArticleTile.classList.add("tile", "is-child", "is-12", "box")
 
     const articleTitle = document.createElement("p")
     articleTitle.classList.add("title")
     articleTitle.dataset.id = article.id
     articleTitle.innerText = article.title
 
+    const articleAuthor = document.createElement("p")
+    articleAuthor.classList.add("subtitle")
+    articleAuthor.dataset.id = article.id
+    articleAuthor.innerText = article.author
+
     const articleSource = document.createElement("p")
     articleSource.classList.add("subtitle")
     articleSource.dataset.id = article.id
     articleSource.innerText = article.source
+
+    const articleImage = document.createElement("img")
+    articleImage.src = article.img_url
     
     const articleDesc = document.createElement("p")
     articleDesc.dataset.id = article.id
@@ -96,12 +144,10 @@ const createArticleTile = article => {
     articleDate.dataset.id = article.id
     articleDate.innerText = article.published_at
 
-    newTile.append(articleTitle, articleSource, articleDesc, articleDate)
-    selectedColumn.append(newTile)
-} 
-
-const createArticleView = article => {
-
+    selectedArticleTile.append(articleTitle, articleAuthor, articleSource, articleImage, articleDesc, articleDate)
+    articleLink.append(selectedArticleTile)
+    articleView.append(articleLink)
+    articleView.scrollIntoView(true)
 }
 
 // Remove all Children of Element
@@ -114,12 +160,23 @@ const clearChildren = element => {
 }
 
 // Event Handlers
-const handleTopicClick = (event) => {
+const handleTopicClick = event => {
     const selectedTopic = cachedTopics.find(topic => {
         return parseInt(topic.id) === parseInt(event.target.dataset.id)
     })
     clearChildren(articleList)
     createArticleList(selectedTopic)
+}
+
+const handleArticleClick = event => {
+    const selectedTopic = cachedTopics.find(topic => {
+        return parseInt(topic.id) === parseInt(event.target.dataset.topicId)
+    })
+    const selectedArticle = selectedTopic.articles.find(article => {
+        return parseInt(article.id) === parseInt(event.target.dataset.id)
+    })
+    clearChildren(articleView)
+    createArticleView(selectedArticle)
 }
 
 // Fetches
